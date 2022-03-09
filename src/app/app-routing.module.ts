ac8b878fa5b8aeecde9d7dec60f5264362ec59cf
Khamzat <xamzatseynaro@gmail.com>
2022-03-09 21:19:52 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./auth/components/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = []
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
