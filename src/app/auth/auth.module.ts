5870e6e99604cbe9960457fa838a9a1914bb7c98
Khamzat <xamzatseynaro@gmail.com>
2022-03-15 23:03:09 +0300

    add auth.module info

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule, Routes} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
+import {RegisterComponent} from "./components/register/register.component";
+import {StoreModule} from "@ngrx/store";
+import {reducers} from "./store/reducers";
+import {AuthService} from "./services/auth.service";
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)],
+  declarations: [RegisterComponent],
+  providers: [AuthService]
+})
+export class AuthModule {
+}
